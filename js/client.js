b502a46e676ae84690f7ab55a2f87dc71636956e
David Reeves <davidsreeves1@gmail.com>
2019-06-20 11:51:24 -0500

    second commit, added click event and mouseenter event

--- js/client.js
+++ js/client.js
@@ -11,14 +11,29 @@ $(document).ready(onReady);
 //When the document is ready, runs the renderStudentList function, displaying each item on 
 //the DOM in an unordered list.
 function onReady(){
+    $('#students').on('click', '.student', showAwesomeAlert);
     renderStudentList(playfair);
-    $('#students').on('mouseenter', event => {
+    highlightRed();
+   // $('#students').on('click', '.student', showAwesomeAlert);
+}
+
+// This function highlights the students name in red once the mouse goes over it.
+function highlightRed() {
+    $('#students').on('mouseenter', '.student', event => {
         $(event.currentTarget).css('color', 'red');
-    }).on('mouseleave', () => {
+    }).on('mouseleave', '.student', event => {
         $(event.currentTarget).css('color', 'black')
     })
 }
 
+// This function will be called when we click on a student.
+function showAwesomeAlert(event){
+    console.log(event.currentTarget.innerHTML);
+    console.log(this);
+    alert(`You're awesome ${event.currentTarget.innerHTML}!`);
+}
+
+// It will show an alert saying that student is awesome!
 //Loops through an array, calling renderStudent(lines 25-28) on each iteration of the array.
 function renderStudentList(studentList) {
     for( let student of studentList ) {
@@ -29,6 +44,6 @@ function renderStudentList(studentList) {
 
 //Takes an argument of an item to be displayed on the DOM.
 function renderStudent(student) {
-    $('#students').append(`<li>${student}</li>`)
+    $('#students').append(`<li class="student">${student}</li>`)
 }
     
